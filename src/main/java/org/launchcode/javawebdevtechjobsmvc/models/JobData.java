3c2547c8efed13e5ef2cae1b58fdebefe20c5a83
parhamsamee <hallsamee@gmail.com>
2021-03-12 13:30:44 -0600

    view all link completed

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
@@ -19,7 +19,7 @@ public class JobData {
     private static final String DATA_FILE = "job_data.csv";
     private static boolean isDataLoaded = false;
 
-    private static ArrayList<Job> allJobs;
+    private static ArrayList<Job> allJobs = new ArrayList<>();
     private static ArrayList<Employer> allEmployers = new ArrayList<>();
     private static ArrayList<Location> allLocations = new ArrayList<>();
     private static ArrayList<PositionType> allPositionTypes = new ArrayList<>();
@@ -203,6 +203,12 @@ public class JobData {
         }
     }
 
+    public static ArrayList<Job> getAllJobs() {
+        loadData();
+        allJobs.sort(new NameSorter());
+        return allJobs;
+    }
+
     public static ArrayList<Employer> getAllEmployers() {
         loadData();
         allEmployers.sort(new NameSorter());
@@ -226,6 +232,7 @@ public class JobData {
         allCoreCompetency.sort(new NameSorter());
         return allCoreCompetency;
     }
+
 
 }
 
